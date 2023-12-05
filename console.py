cc6e8d4fdcc6e7488182ffeb9531b00469cf1f89
Musakalamz <ogunsolumusa@gmail.com>
2023-12-05 14:42:31 -0800

    Console 0.1
    Update your command interpreter (console.py) to have these commands:
    
    create: Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id. Ex: $ create BaseModel
    If the class name is missing, print ** class name missing ** (ex: $ create)
    If the class name doesn’t exist, print ** class doesn't exist ** (ex: $ create MyModel)
    show: Prints the string representation of an instance based on the class name and id. Ex: $ show BaseModel 1234-1234-1234.
    If the class name is missing, print ** class name missing ** (ex: $ show)
    If the class name doesn’t exist, print ** class doesn't exist ** (ex: $ show MyModel)
    If the id is missing, print ** instance id missing ** (ex: $ show BaseModel)
    If the instance of the class name doesn’t exist for the id, print ** no instance found ** (ex: $ show BaseModel 121212)
    destroy: Deletes an instance based on the class name and id (save the change into the JSON file). Ex: $ destroy BaseModel 1234-1234-1234.
    If the class name is missing, print ** class name missing ** (ex: $ destroy)
    If the class name doesn’t exist, print ** class doesn't exist ** (ex:$ destroy MyModel)
    If the id is missing, print ** instance id missing ** (ex: $ destroy BaseModel)
    If the instance of the class name doesn’t exist for the id, print ** no instance found ** (ex: $ destroy BaseModel 121212)
    all: Prints all string representation of all instances based or not on the class name. Ex: $ all BaseModel or $ all.
    The printed result must be a list of strings (like the example below)
    If the class name doesn’t exist, print ** class doesn't exist ** (ex: $ all MyModel)
    update: Updates an instance based on the class name and id by adding or updating attribute (save the change into the JSON file). Ex: $ update BaseModel 1234-1234-1234 email "aibnb@mail.com".
    Usage: update <class name> <id> <attribute name> "<attribute value>"
    Only one attribute can be updated at the time
    You can assume the attribute name is valid (exists for this model)
    The attribute value must be casted to the attribute type
    If the class name is missing, print ** class name missing ** (ex: $ update)
    If the class name doesn’t exist, print ** class doesn't exist ** (ex: $ update MyModel)
    If the id is missing, print ** instance id missing ** (ex: $ update BaseModel)
    If the instance of the class name doesn’t exist for the id, print ** no instance found ** (ex: $ update BaseModel 121212)
    If the attribute name is missing, print ** attribute name missing ** (ex: $ update BaseModel existing-id)
    If the value for the attribute name doesn’t exist, print ** value missing ** (ex: $ update BaseModel existing-id first_name)
    All other arguments should not be used (Ex: $ update BaseModel 1234-1234-1234 email "aibnb@mail.com" first_name "Betty" = $ update BaseModel 1234-1234-1234 email "aibnb@mail.com")
    id, created_at and updated_at cant’ be updated. You can assume they won’t be passed in the update command
    Only “simple” arguments can be updated: string, integer and float. You can assume nobody will try to update list of ids or datetime

--- console.py
+++ console.py
@@ -2,7 +2,11 @@
 
 """Defines the HBnB console."""
 
+import shlex
 import cmd
+from models.base_model import BaseModel
+
+my_classes = {"BaseModel": BaseModel}
 
 
 class HBNBCommand(cmd.Cmd):
@@ -14,14 +18,119 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
 
-    def do_quit(self, arg):
+    def do_quit(self, args):
         """Quit command to exit the program."""
         return True
 
-    def do_EOF(self, arg):
+    def do_EOF(self, args):
         """EOF signal to exit the program."""
         print("")
-        return Tru
+        return True
+
+    def do_create(self, args):
+        """ Usage: create <class>
+        Create a new class instance and print its id.
+        """
+
+        args = shlex.split(args)
+        if not args:
+            print("** class name missing **")
+            return
+        if args[0] not in my_classes:
+            print("** class doesn't exist **")
+            return
+
+    def do_show(self, args):
+        """Usage: show <class> <id> or <class>.show(<id>)
+        Display the string representation of a class instance of a given id.
+        """
+
+        args_list = shlex.split(args)
+        """args_list is a list of arguments passed to the command
+              shlex is a lexical analyser for simple shell-like syntax;
+                and shlex.split() splits a string into a list of tokens."""
+        if not args:
+            print("** class name missing **")
+            return
+        elif args_list[0] not in my_classes:
+            print("** class doesn't exist **")
+            return
+        elif len(args_list) == 1:
+            print("** instance id missing **")
+            return
+        else:
+            print("** no instance found **")
+            return
+
+    def do_destroy(self, args):
+        """Deletes an instance based on the class name and id."""
+        args_list = shlex.split(args)
+        """args_list is a list of arguments passed to the command
+                shlex is a lexical analyser for simple shell-like syntax;
+                and shlex.split() splits a string into a list of tokens."""
+        if len(args_list) == 0:
+            print("** class name missing **")
+            return
+        if len(args_list) == 1:
+            print("** instance id missing **")
+        else:
+            print("** instance id missing **")
+
+    def do_all(self, args):
+        """Prints all string representation of all instances
+        based or not on the class name."""
+
+        
+        list_objects = []
+        """list_objects is a list with all the objects"""
+        if args not in my_classes:
+            """if args is not empty and args is not in my_classes,
+            then the class doesn't exist"""
+            print("** class doesn't exist **")
+            return
+        if args in self.classes:
+            for key, value in new_object.items():
+                """for key, value in new_object.items()
+                    key is the key of the dictionary
+                    value is the value of the dictionary, new_object.items is a
+                    generator that returns the key-value of the dictionary"""
+                if args in key:
+                    """if args is in key, then the class exists"""
+                    toke_key = key.split(".")
+                    """toke_key is a list with the class name and the id"""
+                    key_new = "[" + toke_key[0] + "]"\
+                        + " (" + toke_key[1] + ")"
+                    list_objects.append(key_new + " " + str(value))
+                    """list_objects.append(key_new + " " + str(value))
+                        list_objects is a list with the objects in format
+                        [class name] (id) object"""
+                    print(list_objects)
+
+    def do_update(self, args):
+        """Updates an instance based on the class name and id by adding or
+        updating attribute (save the change into the JSON file)."""
+
+        if args == '':
+            print("** class name missing **")
+            return
+        args_list = args.split()
+        if args_list[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args_list) == 1:
+            print("** instance id missing **")
+            return
+        key = args_list[0] + "." + args_list[1]
+        all_objects = models.storage.all()
+        if key not in all_objects:
+            print("** no instance found **")
+            return
+        if len(args_list) == 2:
+            print("** attribute name missing **")
+            return
+        if len(args_list) == 3:
+            print("** value missing **")
+            return
 
 
 if __name__ == "__main__":
