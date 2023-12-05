001b1bc21c8c5313165a2e870e929149cc415258
Musakalamz <ogunsolumusa@gmail.com>
2023-12-05 00:21:10 -0800

    Create BaseModel from dictionary
    Previously we created a method to generate a dictionary representation of an instance (method to_dict()).
    
    Now it’s time to re-create an instance with this dictionary representation.
    
    <class 'BaseModel'> -> to_dict() -> <class 'dict'> -> <class 'BaseModel'>
    Update models/base_model.py:
    
    __init__(self, *args, **kwargs):
    you will use *args, **kwargs arguments for the constructor of a BaseModel. (more information inside the AirBnB clone concept page)
    *args won’t be used
    if kwargs is not empty:
    each key of this dictionary is an attribute name (Note __class__ from kwargs is the only one that should not be added as an attribute. See the example output, below)
    each value of this dictionary is the value of this attribute name
    Warning: created_at and updated_at are strings in this dictionary, but inside your BaseModel instance is working with datetime object. You have to convert these strings into datetime object. Tip: you know the string format of these datetime
    otherwise:
    create id and created_at as you did previously (new instance)

--- models/base_model.py
+++ models/base_model.py
@@ -11,12 +11,25 @@ from datetime import datetime
 class BaseModel:
     """Represents the BaseModel of HBnB project."""
 
-    def __init__(self):
-        """Initialize a new BaseModel."""
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
 
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+            """
+
+        dtfmt = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+
+        if len(kwargs) != 0:
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(val, dtfmt)
+                else:
+                    self.__dict__[key] = val
 
     def save(self):
         """Update updated_at with the current datetime"""
@@ -29,11 +42,11 @@ class BaseModel:
         the class name of the object.
         """
 
-        rtndict = self.__dict__.copy()
-        rtndict["created_at"] = self.created_at.isoformat()
-        rtndict["updated_at"] = self.updated_at.isoformat()
-        rtndict["__class__"] = self.__class__.__name__
-        return rtndict
+        new_dict = self.__dict__.copy()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
 
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
